ed817d36e942be24953a625277c6e68ec8123f32
F4NT0 <gabriel.stundner@acad.pucrs.br>
2019-10-11 04:11:08 -0300

    created all webpage system but dont work

--- app/src/main/java/com/example/curriculo/MainActivity.java
+++ app/src/main/java/com/example/curriculo/MainActivity.java
@@ -11,57 +11,23 @@ import android.widget.ImageButton;
 import android.view.View.OnClickListener;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
+
+    public static final String github =   "https://github.com/F4NT0";
+    public static final String linkedin = "https://br.linkedin.com/in/gabriel-fanto-stundner-b19723164";
+    public static final String twitter =  "https://twitter.com/GABRIELFANTO";
+    public static final String site =     "https://f4nt0.github.io/PR0GR4M1NG/";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        WebView pages = findViewById(R.id.web);
-
-        ImageButton github = findViewById(R.id.githubButton);
-        github.setOnClickListener(this);
-        ImageButton linkedin = findViewById(R.id.linkedinButton);
-        linkedin.setOnClickListener(this);
-        ImageButton site = findViewById(R.id.siteButton);
-        site.setOnClickListener(this);
-        ImageButton twitter = findViewById(R.id.twitterButton);
-        twitter.setOnClickListener(this);
-
-
     }
 
-
-
-
-    /*
-      ==================================
-      MÉTODOS PARA ABRIR OS LINKS NA WEB
-      ==================================
-     */
-
-    public void linkgithub(View v) {
-//        Intent github = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/F4NT0"));
-//        startActivity(github);
-
-    }
-    public void linkLinkedin(View v){
-        Intent linkedin = new Intent(Intent.ACTION_VIEW, Uri.parse("https://br.linkedin.com/in/gabriel-fanto-stundner-b19723164"));
-        startActivity(linkedin);
-    }
-    public void linkTwitter(View v){
-        Intent twitter = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/GABRIELFANTO"));
-        startActivity(twitter);
-    }
-    public void linkSite(View v){
-        Intent site = new Intent(Intent.ACTION_VIEW, Uri.parse("https://f4nt0.github.io/PR0GR4M1NG/"));
-        startActivity(site);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch()
+    //Criando e iniciando os Links nas Paginas
+    public void loadWebPage(View v){
+        WebViewFragment webViewFragment = new WebViewFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.pageWeb,webViewFragment).commit();
     }
 }
